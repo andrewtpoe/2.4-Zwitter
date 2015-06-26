cdc97d6eaa5c7bee8dcaaeec5b99296f7d99a3bb
Andrew T. poe <techapoe@gmail.com>
2015-06-25 23:01:08 -0400

    Added a couple new methods. Got Zombie.delete_prey working. Created Zombie.find_zombie(username). Accepts username, returns class instance or nil.

--- classes/zombie.rb
+++ classes/zombie.rb
@@ -1,11 +1,26 @@
 class Zombie
   attr_accessor :name, :username, :password, :image, :location, :bio, :created_at, :tweets, :next_tweet_id, :stalkers, :prey
 
+  @@instances = []
+
   def initialize
     self.next_tweet_id = 0
     self.tweets = []
     self.stalkers = []
     self.prey = []
+    @@instances << self
+  end
+
+  # Create class method that accepts username argument.
+  # Returns instance of class with correct username.
+  def self.find_zombie(username)
+    matching_zombie = nil
+    @@instances.each do | zombie |
+      if zombie.username == username
+        matching_zombie = zombie
+      end
+    end
+    matching_zombie
   end
 
   def create_tweet(content:, location: nil)
@@ -23,17 +38,20 @@ class Zombie
     self.tweets.delete_if { | tweet | tweet.tweet_id == tweet_id }
   end
 
-  def add_prey(zombie)
+  def add_prey(username)
+    zombie = Zombie.find_zombie(username)
     self.prey.push(zombie)
     zombie.stalkers.push(self)
   end
 
+  # Should delete the username passed in from the prey list,
+  # and remove this instance from that prey's stalker list.
   def delete_prey(username)
     self.prey.delete_if do | victim |
       if victim.username == username
         victim.stalkers.delete_if { | lurker | lurker.username == @username }
       end
     end
-
   end
+
 end
--- tests/test_zombie.rb
+++ tests/test_zombie.rb
@@ -3,27 +3,38 @@ require_relative '../app.rb'
 
 class TestZombie < MiniTest::Test
   def setup
+    @a_zombie = Zombie.new
+    @a_zombie.username = "a_zombie"
+    @prey = Zombie.new
+    @prey.username = "***PREY***"
+    @stalker = Zombie.new
+    @stalker.username = "***STALKER***"
+  end
+
+  def test_class_method_find_user_by_username_returns_correct_user
+    found_zombie = Zombie.find_zombie("a_zombie")
+    assert_equal "a_zombie", found_zombie.username
+  end
+
+  def test_class_method_find_user_by_username_returns_nil_if_no_match_found
+    found_zombie = Zombie.find_zombie("BwainEater")
+    assert_equal nil, found_zombie
   end
 
   def test_self_deleted_from_preys_stalker_list
-    prey = Zombie.new
-    prey.username = "***PREY***"
-    stalker = Zombie.new
-    stalker.username = "***STALKER***"
-    stalker.prey.push(prey)
-    prey.stalkers.push(stalker)
-    stalker.delete_prey(prey)
-    # Check to see if zombie1 has a stalker zombie2
+    @stalker.prey.push(@prey)
+    @prey.stalkers.push(@stalker)
+    @stalker.delete_prey("***PREY***")
     has_stalker = false
-    prey.stalkers.each do | stalker |
+    @prey.stalkers.each do | stalker |
       if stalker.username == "***STALKER***"
-        puts "THIS STALKER EXISTS"
+#        puts "THIS STALKER EXISTS"
         has_stalker = true
       end
     end
-    puts prey.stalkers.inspect
-    puts stalker.prey.inspect
-
+    # puts "Prey's stalkers: #{prey.stalkers.inspect}"
+    # puts ""
+    # puts "Stalker's prey: #{stalker.prey.inspect}"
     assert_equal false, has_stalker
   end
 end
